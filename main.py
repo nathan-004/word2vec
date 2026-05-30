90064f788cb97e9a12d28dcc8ecafd3f9cd7fa9c
Nathan <n49874687@gmail.com>
2026-05-30 16:21:38 +0200

    feat: entraînement modèle SkipGram

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 import string
 from typing import Optional
 
+import torch
+import torch.nn as nn
+
 def get_phrases(text) -> list[str]:
     """Renvoie une liste du texte contenu dans chaque phrases"""
     start = 0
@@ -51,15 +54,60 @@ def get_vocabulary(training_data: list[tuple[str, str]]) -> list:
         [pair[0] for pair in training_data]
     ))
 
-text_corpus = "Ceci est un test."
+text_corpus = """Je ne connaîtrai pas la peur car la peur tue l’esprit.
+La peur est la petite mort qui conduit à l’oblitération totale. 
+J’affronterai ma peur. Je lui permettrai de passer sur moi, au travers de moi. Et lorsqu’elle sera passée, je tournerai mon œil intérieur sur son chemin. Et là où elle sera passée, il n’y aura plus rien. Rien que moi."""
 VOCABULARY = get_vocabulary(get_training_samples(text_corpus))
 print(VOCABULARY)
 
-def get_vector(word: str, vocab: Optional[list] = None) -> list:
+pairs = get_training_samples(text_corpus)
+
+def get_vector(word: str, vocab: Optional[list] = None) -> torch.Tensor:
     """Retourne un vecteur 1, len(vocab) avec toutes les valeurs nulles sauf celle correspondant au mot donné"""
     if vocab is None:
         vocab = VOCABULARY
 
-    return [int(val == word) for val in vocab]
+    return torch.tensor(
+        [int(val == word) for val in vocab],
+        dtype=torch.float
+    )
 
-print(get_vector("test"))
+class SkipGram(nn.Module):
+    def __init__(self, embedding_dim: int, vocabulary: Optional[list] = None):
+        super().__init__()
+
+        if vocabulary is None:
+            vocabulary = VOCABULARY
+
+        self.vocabulary = vocabulary
+        n = len(vocabulary)
+
+        self.embeddings = nn.Embedding(n, embedding_dim)
+        self.linear = nn.Linear(embedding_dim, n)
+
+    def forward(self, x: list | torch.Tensor):
+        x = self.embeddings(x)
+        x = self.linear(x)
+
+        return x
+
+X = torch.tensor([VOCABULARY.index(p[0]) for p in pairs])
+Y = torch.tensor([VOCABULARY.index(p[1]) for p in pairs])
+
+model = SkipGram(10)
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+for epoch in range(1000):
+    logits = model(X)
+
+    loss = loss_fn(logits, Y)
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    print(loss.item(), end="\r")
+    
+print()
